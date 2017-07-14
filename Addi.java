5ae9a7a0c5f17606e87e6c1c9b48196e8c26fbec
suraj <surajbaviskar30@gmail.com>
2017-07-14 15:47:13 -0700

    suraj

---
+++ Addi.java
@@ -0,0 +1,20 @@
+class Add 
+{
+ 
+    int addnumber(int number1, int number2)
+   {    
+         return (number1+number2);
+         
+         
+         
+   }
+   public staic void main(Strigs args[])
+   {
+   
+       Add add=new Add();
+       int addition=add.addnumber(10,20);
+       System.out.println("Addition is"+addition);
+   
+   
+   }
+}  
